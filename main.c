48817b54c5beb3f50e2d4e83ac6861654162375f
root <root@localhost.localdomain>
2018-09-10 18:27:02 -0400

    added last nights final work

--- main.c
+++ main.c
@@ -1,106 +1,222 @@
-/********************************************************************************
-                                Includes
-********************************************************************************/
+
+#include<stdint.h>
+#include<stdlib.h>
+
 #include <avr/io.h>
-#include <stdio.h>
-#include <stdbool.h>
+#include <util/delay.h>
 
-/********************************************************************************
-                                Macros and Defines
-********************************************************************************/
-#define F_CPU 8000000
-#define BAUD 9600
-#define MYUBRR F_CPU/16/BAUD-1
+void setup();
+void loop();
+void sensor_data();
 
-/********************************************************************************
-                                Function Prototypes
-********************************************************************************/
-void usart_init(uint16_t ubrr);
-char usart_getchar( void ); 
-void usart_putchar( char data ); 
-void usart_pstr(char *s);
-unsigned char usart_kbhit(void);
-int usart_putchar_printf(char var, FILE *stream);
+uint16_t ping_1;
+uint16_t ping_2;
+uint16_t ping_3;
+float pr_1;
+float pr_2;
+uint16_t ir_1;
+uint16_t ir_2;
+uint8_t obstacle_front;
+uint8_t obstacle_left;
+uint8_t obstacle_right;
+uint8_t inv_wall_left;
+uint8_t inv_wall_right;
+uint8_t in_room;
+uint8_t state;
+uint8_t prev_direction;
 
-/********************************************************************************
-Global Variables
-********************************************************************************/
-static FILE mystdout = FDEV_SETUP_STREAM(usart_putchar_printf, NULL, _FDEV_SETUP_WRITE);
-
-/********************************************************************************
-                                Main
-********************************************************************************/
-int main( void )
+int main()
 {
-	uint8_t myvalue;
+  setup();
 
-	// setup our stdio stream
-	stdout = &mystdout;
+  while(1)
+  {
+    loop();
+  }
 
-	// fire up the usart
-	usart_init ( MYUBRR );
-
-	// dump some strings to the screen at power on
-	myvalue = 64;
-	printf("Here is myvalue as an unsigned integer: %d\n", myvalue);
-	printf("Here is myvalue as a char: %c\n", myvalue);
-	printf("Here is myvalue in hex: 0x%X\n", myvalue);
-	printf("Here is myvalue in octal: %o\n", myvalue);
-
-	// main loop
-	while(true) 
-	{
-		// do nothing
-	}
+  return 0;
 }
 
-/********************************************************************************
-                                usart Related
-********************************************************************************/
-void usart_init( uint16_t ubrr) {
-	// Set baud rate
-	UBRR0H = (uint8_t)(ubrr>>8);
-	UBRR0L = (uint8_t)ubrr;
-	// Enable receiver and transmitter
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-	// Set frame format: 8data, 1stop bit
-	UCSR0C = (3<<UCSZ00);
+
+void setup(){
+  DDRB = 0x0F;
+  ping_1 = 0;
+  ping_2 = 0;
+  ping_3 = 0;
+  pr_1 = 0.0f;
+  pr_2 = 0.0f;
+  ir_1 = 0;
+  ir_2 = 0;
+  obstacle_left = 0;
+  obstacle_front = 0;
+  obstacle_right = 0;
+  inv_wall_left = 0;
+  inv_wall_right = 0;
+  in_room = 0;
+  state = 0;
+  prev_direction = 0;
 }
 
-void usart_putchar(char data) { 
-	// Wait for empty transmit buffer
-	while ( !(UCSR0A & (_BV(UDRE0))) );
-	// Start transmission
-	UDR0 = data; 
+void loop(){
+
+  sensor_data();
+	PORTB = 0x00;
+
+	if((state & 0x10)==0x10)
+		state = state | 0x0F;
+	else
+		state=0x00;
+	state = obstacle_front | obstacle_left | obstacle_right | inv_wall_left | inv_wall_right | in_room;
+	PORTB = state;
+
+  if(state >= 0x20)
+    state = 0x20;
+
+  switch (state) {
+
+    case 0x00:
+
+    break;
+
+    case 0x01:
+
+    break;
+
+    case 0x02:
+
+    break;
+
+    case 0x03:
+
+    break;
+
+    case 0x04:
+
+    break;
+
+    case 0x05:
+
+    break;
+
+    case 0x06:
+
+    break;
+
+    case 0x07:
+
+    break;
+
+    case 0x08:
+
+    break;
+
+    case 0x09:
+
+    break;
+
+    case 0x0A:
+
+    break;
+
+    case 0x0B:
+
+    break;
+
+    case 0x0C:
+
+    break;
+
+    case 0x0D:
+
+    break;
+
+    case 0x0E:
+
+    break;
+
+    case 0x0F:
+
+    break;
+
+    case 0x10:
+
+    break;
+
+    case 0x11:
+
+    break;
+
+    case 0x12:
+
+    break;
+
+    case 0x13:
+
+    break;
+
+    case 0x14:
+
+    break;
+
+    case 0x15:
+
+    break;
+
+    case 0x16:
+
+    break;
+
+    case 0x17:
+
+    break;
+
+    case 0x18:
+
+    break;
+
+    case 0x19:
+
+    break;
+
+    case 0x1A:
+
+    break;
+
+    case 0x1B:
+
+    break;
+
+    case 0x1C:
+
+    break;
+
+    case 0x1D:
+
+    break;
+
+    case 0x1E:
+
+    break;
+
+    case 0x1F:
+
+    break;
+
+    case 0x20:
+
+    break;
+  }
+
+
+  _delay_ms(1000);
 }
 
-char usart_getchar(void) { 
-	// Wait for incomming data
-	while ( !(UCSR0A & (_BV(RXC0))) );
-	// Return the data
-	return UDR0;
-} 
 
-unsigned char usart_kbhit(void) {
-	//return nonzero if char waiting polled version
-	unsigned char b;
-	b=0;
-	if(UCSR0A & (1<<RXC0)) b=1;
-	return b;
-}
-
-void usart_pstr(char *s) {
-    // loop through entire string
-	while (*s) {  
-        usart_putchar(*s);
-        s++;
-    }
-}
-
-// this function is called by printf as a stream handler
-int usart_putchar_printf(char var, FILE *stream) {
-	// translate \n to \r for br@y++ terminal
-	if (var == '\n') usart_putchar('\r');
-	usart_putchar(var);
-	return 0;
+void sensor_data(){
+  //obstacle_front = 0b00000001;
+  //obstacle_left = 0b00000010;
+  //obstacle_right = 0b00000100;
+  //inv_wall_left = 0b00001000;
+  //inv_wall_right = 0b00010000;
+  //in_room=0b00100000;
 }
--- setup.bash
+++ setup.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 src=main
+src_files="main.c"
 baud=9600
-programmerDev=/dev/bus/usb/005/001
+programmerDev=usb
 programmerType=avrisp2
 avrType=atmega328p
 avrFreq=1843200
@@ -10,7 +11,11 @@ avrFreq=1843200
 
 cflags="-g -DF_CPU=${avrFreq} -Wall -Os -Werror -Wextra"
 
-avr-gcc ${cflags} -mmcu=${avrType} -Wa,-ahlmns=${src}.lst -c -o ${src}.o ${src}.c
+avr-gcc ${cflags} -mmcu=${avrType} -Wa,-ahlmns=${src}.lst -c -o ${src}.o ${src_files}
+
+avr-gcc ${cflags} -mmcu=${avrType} -o ${src}.elf ${src}.o
+
+avr-objcopy -j .text -j .data -O ihex ${src}.elf ${src}.flash.hex
 
 avrdude -p ${avrType} -c ${programmerType} -P ${programmerDev} -b ${baud} -v -U flash:w:${src}.flash.hex
 
