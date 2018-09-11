66dad42abb763dda1f7995280364514220b01b5c
William Born <wborn@localhost.localdomain>
2018-09-11 19:57:28 -0400

    fixed DEFINE statements

--- sensors/photo.c
+++ sensors/photo.c
@@ -1,9 +1,11 @@
+#DEFINE DARK 309
+#DEFINE LIGHT 310
+
+
 int light_value;
 int roomlight_value;
 int lightPin = 0;         //Pin assignment
-int roomlightPin = 1;     //Pin assignment 
-#DEFINE DARK 309
-#DEFINE LIGHT 310
+int roomlightPin = 1;     //Pin assignment
 
 void setup() {
 
@@ -12,17 +14,17 @@ void setup() {
 }
 
 void loop() {
-  
+
   light_sensor();
   set_flags();
 
 }
 
 int light_sensor(){
-  
+
   roomlight_value = (analogRead(roomlightPin)); //Write the value of the photoresistor to the room light variable.
-  light_value = (analogRead(lightPin)); //Write the value of the photoresistor to the light variable 
-  
+  light_value = (analogRead(lightPin)); //Write the value of the photoresistor to the light variable
+
 }
 
 int set_flags(){
@@ -31,5 +33,5 @@ int set_flags(){
     inroom = 0b00100000;   //If robot does not detect light (i.e. room is dark) set flag to 1
   if (light_value => LIGHT)
     inroom = 0b00000000;  //If the robot detects light, set the flag to 0
-    
+
 }
